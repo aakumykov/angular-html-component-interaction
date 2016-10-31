70df5b5143b43d382751a463b73a4eea683271eb
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-31 11:59:44 +0700

    showEventInfo()

--- index.js
+++ index.js
@@ -12,13 +12,37 @@ window.addEventListener('load', function(){
 
 function initializeEditor(){
 	console.log('initializeEditor()');
-	//var editor = angular.element('#editor');
-	var editor = document.getElementById('editor');
-		editor.addEventListener('click', function(e){
-		console.debug(e.target);
-		console.debug('e.screenX: '+e.screenX);
-		console.debug('e.screenY: '+e.screenY);
-		console.debug('e.clientX: '+e.clientX);
-		console.debug('e.clientY: '+e.clientY);
+	
+	var editor = angular.element('#editor');
+
+	editor.mouseDispatsher = {
+		status: {
+			startTarget: undefined,
+			finishTarge: undefined,
+			eventType: undefined,
+		},
+		startWatch: function(e){
+			console.log('mouseDispatsher.startWatch()');
+
+		},
+		endWatch: function(e){
+			console.log('mouseDispatsher.endWatch()');
+		}
+	}
+
+	editor.showEventInfo = function(ev){
+		console.log('---------------- event info ----------------')
+		for (var key in ev) {
+			var value = ev[key];
+			var valueType = typeof(value);
+			if ('string'==valueType || 'number'==valueType || 'boolean'==valueType) {
+				console.log(key+': '+value);
+			}
+		}
+		console.log('---------------- event info ----------------')
+	}
+
+	editor.on('click', function(e){
+		editor.showEventInfo(e);
 	});
 }
