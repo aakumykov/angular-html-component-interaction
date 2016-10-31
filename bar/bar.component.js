ddf3dfda70bfe884805a8e1d885c4115c71b4c0a
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-31 15:46:29 +0700

    Мышиные пробы в диспетчере.

--- bar/bar.component.js
+++ bar/bar.component.js
@@ -1,13 +1,29 @@
 'use strict';
 
 function BarController(dispatcher){
-	//this.name = 'Прямоугольник';
 	this.name = dispatcher.getName();
+	this.test1 = function(){ console.log('BarController.test1()'); }
+	
+	angular.element('bar').on('mousedown', function(ev){
+		dispatcher.mousedown(ev);
+	});
+	angular.element('bar').on('mousemove', function(ev){
+		dispatcher.mousemove(ev);
+	});
+	angular.element('bar').on('mouseup', function(ev){
+		dispatcher.mouseup(ev);
+	});
 }
 
 angular.module('Bar',[]).
 component('bar',{
-	template: `<div class='bar'>{{barCtrl.name}}</div>`,
+	template: `
+		<div 
+			id='bar' 
+			class='bar' 
+			//ng-click='barCtrl.test1()' 
+		>{{barCtrl.name}}</div>
+	`,
 	controller: BarController,
 	controllerAs: 'barCtrl',
 });
--- bar/bar.service.js
+++ bar/bar.service.js
@@ -2,8 +2,48 @@
 
 angular.module('Bar').
 factory('dispatcher', function(){
-	this.name = 'Диспетчер';
 	var self = this;
+	
+	this.name = 'Диспетчер';
 	this.getName = function(){ return self.name; }
+
+	this.pressed = false;
+
+	this.info = {
+		start: {
+			target: undefined,
+			x: undefined,
+			y: undefined,
+		},
+		end: {
+			target: undefined,
+			x: undefined,
+			y: undefined,
+		},
+		path: [],
+	}
+
+	this.mousedown = function(ev){
+		console.log('dispatcher.mousedown()');
+		self.pressed = true;
+		self.info.start.target = ev.target;
+		self.info.start.x = ev.clientX;
+		self.info.start.y = ev.clientY;
+	}
+	this.mousemove = function(ev){
+		if (self.pressed) console.log('dispatcher.mousemove('+ev.clientX+','+ev.clientY+')');
+		// self.info.path.push({
+		// 	x: ev.clientX, 
+		// 	y: ev.clientY
+		// })
+	}
+	this.mouseup = function(ev){
+		console.log('dispatcher.mouseup()');
+		self.pressed = false;
+		self.info.end.target = ev.target;
+		self.info.end.x = ev.clientX;
+		self.info.end.y = ev.clientY;
+	}
+
 	return this;
 });
