102868997f8689290e99e665d5a5be0c3cfe15a6
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-31 10:24:17 +0700

    Макет страницы.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 window.addEventListener('load', function(){
-	if ( angular.bootstrap(document, ['MyApp']) ) {
+	if ( angular.bootstrap(document, ['App']) ) {
 		console.debug('Программа загружена.');
 	} else {
 		console.debug('Ошибка инициализации приложения.');
