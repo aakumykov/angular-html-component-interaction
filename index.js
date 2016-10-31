e3c7a9fa9199ea2cf032fbd0cc06597f60ccbbc0
Андрей Кумыков <aakumykov@yandex.ru>
2016-10-31 10:54:57 +0700

    Начало проб с событиями.

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ window.addEventListener('load', function(){
 	} else {
 		console.debug('Ошибка инициализации приложения.');
 	}
+
+	initializeEditor();
 });
+
+function initializeEditor(){
+	console.log('initializeEditor()');
+	//var editor = angular.element('#editor');
+	var editor = document.getElementById('editor');
+		editor.addEventListener('click', function(e){
+		console.debug(e.target);
+		console.debug('e.screenX: '+e.screenX);
+		console.debug('e.screenY: '+e.screenY);
+		console.debug('e.clientX: '+e.clientX);
+		console.debug('e.clientY: '+e.clientY);
+	});
+}
